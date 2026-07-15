edc1e2cf579e6048509e217b793d4bdb0d0a6cbd
ramphy <ramphy_an@outlook.com>
2026-07-15 23:23:23 +0200

    update: first impl of home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ROUTES } from '../../app.routes';
 
 @Component({
 	selector: 'app-home',
-	imports: [],
+	imports: [RouterLink],
 	templateUrl: './home.html',
 	styleUrl: './home.css',
 	host: { class: 'page' },
 })
-export class Home {}
+export class Home {
+	readonly SEC_PRIVACY_ROUTE = ROUTES.SEC_PRIVACY;
+	readonly DOWNLOAD_ROUTE = ROUTES.DOWNLOAD;
+}
